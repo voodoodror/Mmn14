f2352c41c89f2ca97495dd2540f4128af0a1febc
voodoodror <voodoodror@gmail.com>
2016-03-06 16:09:37 +0200

    ..

--- Dror/src/assembler.h
+++ Dror/src/assembler.h
@@ -10,3 +10,11 @@
 
 void first_parsing_line (char *line, int *count);
 char *hasSymbol(char* str);
+char *getSymbol(char* str);
+
+typedef struct symbolList {
+	char *Sym;
+	unsigned int addrSym;
+	int extSym;
+	int actionSym;
+};
