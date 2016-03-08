c5876a79f564368de907f3b5bb97c5e866b73f86
Dror Bletter <voodoodror@gmail.com>
2016-03-08 19:47:13 +0200

    Update assembler.c

--- Dror/src/assembler.c
+++ Dror/src/assembler.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "assembler.h"
 
-	mySymbolList *symbolList;
     char *lineToProcess;
     char symbolChar = ':';
+    char dotChar = '.';
+    char spaceChar = ' ';
+    int symbolLen = 0;
+    int symbolCounter = 0;
+    char *tmpSymbol;
+    mySymbolList* symbolList;
 
 int main(int argc, char **argv)
 {
@@ -16,7 +22,7 @@ int main(int argc, char **argv)
 
     for (i = 1; i < argc; i++)
     {
-        int count = 0, buf_count = BUF_SIZE;
+        int count = 0;
 
         sprintf(file_name, "%s", argv[i]);
         fp = fopen(file_name, "r");
@@ -27,67 +33,163 @@ int main(int argc, char **argv)
             return -1;
         }
 
+        mySymbolList* symbolList = createSymbolNode("test");
+
         while (fgets(lineToProcess, MAX_LINE, fp))
         {
         	first_parsing_line(lineToProcess, &count);
         	count++;
         }
         fclose(fp);
+        mySymbolList* iter;
+		for (iter = symbolList; NULL != iter; iter = iter->next) {
+				printf("%s ", iter->Sym);
+		}
     }
     return 0;
 }
 
 void first_parsing_line (char *line, int *count) {
-	int i=0, ic=0, dc=0;
+	int i=0, dupSymbol=0;
 
-	mySymbolList *symbolList = malloc(sizeof(mySymbolList));
-
+	char *symbolPointer;
 
 	strip_extra_spaces(line);
 	if (line[0] == ';') {
-		printf("%d: %s (ignoring)\n",*count,line);
+		printf("%d: %s (comment, ignoring)\n",*count,line);
 	} else {
-		if (hasSymbol(line) != NULL) {
-				if (hasSymbol(hasSymbol(line)+1) != NULL) {
+		if (!isalpha(line[0]) && line[0] != '.') {
+			printf("%d: %s (must begin with a letter OR a DOT!)\n",*count,line);
+		} else {
+			if (hasSymbol(line) != 0) {
+				if (hasSymbol(line+(symbolLen+1)) != 0) {
 					printf("%d: %s (multiple symbols, ignoring)\n",*count,line);
 				} else {
-					/*if (symbolList != NULL) {
-						mySymbolList *tmp = realloc(symbolList,sizeof(symbolList)+sizeof(mySymbolList));
-						if (tmp == NULL) {
-							printf("No memory left for new symbol.");
-							exit(0);
+					if (!dupSymbol) {
+						if (hasDot(line+(symbolLen+2)) != NULL) {
+							if (strcmp(getDotInstruction(line+(symbolLen+3)),"string")) {
+								printf("%d: %s (string found)\n",*count,line);
+							}
 						}
-						else {
-							symbolList = tmp;*/
-							symbolList[0].Sym = getSymbol(line);
-						/*}
-						}*/
+					printf("%d: %s (1 valid symbol found)\n",*count,line);
+
+					symbolPointer = getSymbol(line,hasSymbol(line));
+					symbolList = addSymbolNode(symbolList, symbolPointer);
+					symbolCounter++;
+					}
 				}
-		} else {
-			printf("%d: %s\n",*count,line);
+			} else {
+					/*switch (findDotInstruction(line))) {
+						case "extern":
+							printf("It's an external!");
+							break;
+						case "entry":
+							printf("It's an entry!");
+							break;
+					}*/
+					printf("%d: %s (no symbols found)\n",*count,line);
+			}
 		}
 	}
 }
 
 void strip_extra_spaces(char* str) {
   int i,x;
-  for(i=x=0; str[i]; ++i)
-    if(!isspace(str[i]) || (i>0 && !isspace(str[i-1])))
-      str[x++] = str[i];
   char newlineChar = '\n';
+  for(i=x=0; str[i]; ++i) {
+    if(!isspace(str[i]) || (i>0 && !isspace(str[i-1])))
+    	str[x++] = str[i];
+    if (str[i+1] == '\t')
+    	str[i+1] = ' ';
+  }
+
   if (strchr(str,newlineChar) != NULL) {
 	  str[x-1] = '\0';
+  } else {
+	  str[x] = '\0';
   }
-  str[x] = '\0';
 }
 
-char *hasSymbol(char* str) {
+int hasSymbol(char* str) {
 	char *symbolPos = strchr(str, symbolChar);
-	return symbolPos;
+	int tmpLen;
+
+	if (symbolPos == NULL)
+		return 0;
+	tmpLen = strlen(str)-strlen(symbolPos);
+	if (!tmpLen)
+		tmpLen+=1;
+
+	symbolLen=tmpLen;
+	return tmpLen;
 }
-char *getSymbol(char* str) {
+char *hasDot(char* str) {
+	char *tmpStr;
+	tmpStr = strchr(str,dotChar);
+
+	return tmpStr;
+}
+int extractData(char* str) {
+	char *symbolPos = strchr(str, symbolChar);
+	int tmpLen;
+
+	if (symbolPos == NULL)
+		return 0;
+	tmpLen = strlen(str)-strlen(symbolPos);
+	if (!tmpLen)
+		tmpLen+=1;
+
+	symbolLen=tmpLen;
+	return tmpLen;
+}
+char *getDotInstruction(char* str) {
+	char *symbolPos = strchr(str, spaceChar);
 	char *tmpSymbol;
-	int symbolLen = strlen(str)-strlen(strchr(str,symbolChar)-1);
-	strncpy(tmpSymbol, str, symbolLen);
+	int tmpLen;
+
+	if (symbolPos == NULL)
+		return 0;
+	tmpLen = strlen(str)-strlen(symbolPos);
+	if (!tmpLen)
+		tmpLen+=1;
+
+	symbolLen=tmpLen;
+
+	strncpy(tmpSymbol,str,tmpLen);
+	tmpSymbol[tmpLen] = '\0';
 	return tmpSymbol;
+}
+int extractString(char* str) {
+	char *symbolPos = strchr(str, symbolChar);
+	int tmpLen;
+
+	if (symbolPos == NULL)
+		return 0;
+	tmpLen = strlen(str)-strlen(symbolPos);
+	if (!tmpLen)
+		tmpLen+=1;
+
+	symbolLen=tmpLen;
+	return tmpLen;
+}
+char *getSymbol(char* str, int pos) {
+	char *tmpSymbol;
+	strncpy(tmpSymbol,str,pos);
+	tmpSymbol[pos] = '\0';
+	return tmpSymbol;
+}
+mySymbolList *createSymbolNode (char* str) {
+	mySymbolList* newSymbol = malloc(sizeof(mySymbolList));
+	if (NULL != newSymbol){
+		newSymbol->Sym = str;
+		newSymbol->next = NULL;
+	}
+	return newSymbol;
+}
+mySymbolList *addSymbolNode (mySymbolList* symbols, char* str) {
+	mySymbolList* newSymbol = createSymbolNode(str);
+	if (NULL != newSymbol) {
+			newSymbol->next = symbols;
+	}
+	return newSymbol;
 }
