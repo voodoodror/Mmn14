c80ef1a1fdd0b7af572df9d19bd170f095197f12
Dror Bletter <voodoodror@gmail.com>
2016-03-09 20:10:08 +0200

    Update assembler.c

--- Dror/src/assembler.c
+++ Dror/src/assembler.c
@@ -5,12 +5,15 @@
 #include "assembler.h"
 
     char *lineToProcess;
-    char symbolChar = ':';
-    char dotChar = '.';
-    char spaceChar = ' ';
+    const char symbolChar = ':';
+    const char dotChar = '.';
+    const char spaceChar = ' ';
+    const char qmChar = '\"';
     int symbolLen = 0;
     int dotLen = 0;
     int symbolCounter = 0;
+    int errorFlag = 0;
+    int dc=0, ic=0;
     mySymbolList* symbolList;
 
 int main(int argc, char **argv)
@@ -33,56 +36,71 @@ int main(int argc, char **argv)
             return -1;
         }
 
-        mySymbolList* symbolList = createSymbolNode("test");
-
         while (fgets(lineToProcess, MAX_LINE, fp))
         {
         	first_parsing_line(lineToProcess, &count);
         	count++;
         }
         fclose(fp);
-        mySymbolList* iter;
-		for (iter = symbolList; NULL != iter; iter = iter->next) {
-				printf("%s ", iter->Sym);
-		}
     }
     return 0;
 }
 
 void first_parsing_line (char *line, int *count) {
-	int i=0, dupSymbol=0, dc=0, ic=0, symbolFound=0;
+	int i=0, dupSymbol=0, symbolFound=0;
 
 	char *symbolPointer;
-	char *tmp;
 	char *stringFound;
-	
+
 	strip_extra_spaces(line);
 	if (line[0] == ';') {
 		printf("%d: %s (comment line, ignoring)\n",*count,line);
 	} else {
 		if (!isalpha(line[0]) && line[0] != '.') {
-			printf("%d: %s (must begin with a letter OR a DOT!)\n",*count,line);
+				printf("%d: (must begin with a letter OR a DOT!)\n",*count,line);
 		} else {
 			if (hasSymbol(line) != 0) {
-				symbolFound=1;
 				if (hasSymbol(line+(symbolLen+1)) != 0) {
 					printf("%d: %s (multiple symbols found, ignoring)\n",*count,line);
 				} else {
+					if (symbolCounter!=0) {
+						symbolPointer = getSymbol(line,hasSymbol(line));
+						mySymbolList* iter;
+						for (iter = symbolList; NULL != iter; iter = iter->next) {
+								if(strcmp(iter->Sym,symbolPointer) == 0) {
+										dupSymbol = 1;
+										printf("%d: %s (duplicate symbol found, ignoring... addr? %u action? %i ext? %i)\n",*count,line,iter->addr,iter->action,iter->ext);
+								}
+						}
+					}
 					if (!dupSymbol) {
+						symbolFound=1;
 						if (hasDot(line+(symbolLen+2)) != NULL) {
 							if (strcmp(getDotInstruction(line+(symbolLen+3)),"string") == 0) {
-								
-								printf("%d: %s (string found, it is: \"%s\")\n",*count,line,stringFound);
+								/*tmp = strtok(line+(symbolLen+3+dotLen+1),qmChar);
+								while (tmp != NULL) {
+									printf("%s\n",tmp);
+									tmp = strtok(NULL,qmChar);
+								}
+								if (line[symbolLen+3+dotLen+1] == '\"' && hasQM(line+(symbolLen+3+dotLen+1)) != NULL) {
+									stringFound = extractString(line+(symbolLen+3+dotLen+1));
+									printf("%d: %s (string found, it is: \"%s\")\n",*count,line,stringFound);
+									} else {
+										printf("%d: %s (string found but does not end properly)\n",*count,line);
+									}*/
+								printf("%d: %s (string found)\n",*count,line);
 							} else if (strcmp(getDotInstruction(line+(symbolLen+3)),"data") == 0) {
 								printf("%d: %s (data found)\n",*count,line);
 							} else {
 								printf("%d: %s (unknown instruction line, ignoring)\n",*count,line);
 							}
 						}
-					printf("%d: %s (1 valid symbol found)\n",*count,line);
-
+					printf("%d: %s (NEW symbol found)\n",*count,line);
 					symbolPointer = getSymbol(line,hasSymbol(line));
-					symbolList = addSymbolNode(symbolList, symbolPointer);
+					if (symbolCounter == 0)
+						symbolList = createSymbolNode(symbolPointer,100,0,0);
+					else
+						symbolList = addSymbolNode(symbolList,symbolPointer,100,0,0);
 					symbolCounter++;
 					}
 				}
@@ -137,6 +155,12 @@ char *hasDot(char* str) {
 
 	return tmpStr;
 }
+char *hasQM(char* str) {
+	char *tmpStr;
+	tmpStr = strchr(str,qmChar);
+
+	return tmpStr;
+}
 int extractData(char* str) {
 	char *symbolPos = strchr(str, symbolChar);
 	int tmpLen;
@@ -167,7 +191,7 @@ char *getDotInstruction(char* str) {
 	return tmp;
 }
 int extractString(char* str) {
-	char *symbolPos = strchr(str, symbolChar);
+	char *symbolPos = strchr(str, qmChar);
 	int tmpLen;
 
 	if (symbolPos == NULL)
@@ -180,21 +204,33 @@ int extractString(char* str) {
 	return tmpLen;
 }
 char *getSymbol(char* str, int pos) {
-	str[pos] = '\0';
-	return str;
+	char *myStr;
+	memcpy(myStr,str,pos);
+	myStr[pos] = '\0';
+	return myStr;
 }
-mySymbolList *createSymbolNode (char* str) {
+mySymbolList *createSymbolNode (char* str, unsigned int dc, int external, int action) {
 	mySymbolList* newSymbol = malloc(sizeof(mySymbolList));
+	char * myStr = malloc(sizeof(strlen(str)));
+	memcpy(myStr,str,strlen(str));
+
 	if (NULL != newSymbol){
-		newSymbol->Sym = str;
+		newSymbol->Sym = myStr;
+		newSymbol->addr = dc;
+		newSymbol->ext = external;
+		newSymbol->action = action;
 		newSymbol->next = NULL;
 	}
 	return newSymbol;
 }
-mySymbolList *addSymbolNode (mySymbolList* symbols, char* str) {
-	mySymbolList* newSymbol = createSymbolNode(str);
-	if (NULL != newSymbol) {
-			newSymbol->next = symbols;
+mySymbolList *addSymbolNode (mySymbolList* symbolList, char* str, unsigned int dc, int external, int action) {
+	mySymbolList* newSymbol = createSymbolNode(str,dc,external,action);
+	mySymbolList *firstpos = symbolList;
+	if (newSymbol != NULL) {
+			while (symbolList->next != NULL) {
+				symbolList = symbolList->next;
+			}
+			symbolList->next = newSymbol;
 	}
-	return newSymbol;
+	return firstpos;
 }
