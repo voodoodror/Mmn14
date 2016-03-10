04d86702a2149352477c85e67130bb50289171a6
Dror Bletter <voodoodror@gmail.com>
2016-03-10 23:15:25 +0200

    Update assembler.h

--- Dror/src/assembler.h
+++ Dror/src/assembler.h
@@ -8,6 +8,44 @@
 #define MAX_LINE 80
 #define BUF_SIZE 100
 
+#define ERA_WIDTH 2
+#define DREG_WIDTH 3
+#define DADDR_WIDTH 2
+#define SREG_WIDTH 3
+#define SADDR_WIDTH 2
+#define OPCODE_WIDTH 4
+#define GROUP_WIDTH 2
+#define RND_WIDTH 2
+#define NIU_WIDTH 1
+
+#define COMB_OFFSET 0
+#define DREG_OFFSET 2
+#define DADDR_OFFSET 5
+#define SREG_OFFSET 7
+#define SADDR_OFFSET 10
+#define OPCODE_OFFSET 12
+#define TYPE_OFFSET 16
+#define RSVD_OFFSET 17
+
+#define MOV 0
+#define CMP 1
+#define ADD 2
+#define SUB 3
+#define NOT 4
+#define CLR 5
+#define LEA 6
+#define INC 7
+#define DEC 8
+#define JMP 9
+#define BNE 10
+#define RED 11
+#define PRN 12
+#define JSR 13
+#define RTS 14
+#define STOP 15
+
+
+
 typedef struct symbolLists {
 	char *Sym;
 	unsigned int addr;
@@ -16,16 +54,41 @@ typedef struct symbolLists {
 	struct symbolLists* next;
 } mySymbolList;
 
+typedef struct hashTables {
+	unsigned int addr;
+    unsigned int era        : ERA_WIDTH;
+    unsigned int dest_reg   : DREG_WIDTH;
+    unsigned int dest_addr  : DADDR_WIDTH;
+    unsigned int src_reg    : SREG_WIDTH;
+    unsigned int src_addr   : SADDR_WIDTH;
+    unsigned int opcode     : OPCODE_WIDTH;
+    unsigned int group      : GROUP_WIDTH;
+    unsigned int rnd        : RND_WIDTH;
+    unsigned int not_in_use : NIU_WIDTH;
+} myHashTable;
 
+typedef struct commandTables {
+	char *command;
+	int opcode;
+	int srcOperations;
+	int destOperations;
+} myCommandTable;
+
+typedef struct dataTables {
+	char *dc;
+	myCommandTable *command;
+	myHashTable *code;
+} myDataTable;
+
+void init_command_table();
 void first_parsing_line (char *line, int *count);
 void strip_extra_spaces (char* str);
 int hasSymbol(char* str);
 char *hasDot(char* str);
-int extractData(char* str);
-int extractString(char* str);
+void extractData(char* str, char * type);
 char *getSymbol(char* str, int pos);
-char *getDotInstruction(char* str);
+char *getNextString(char* str);
 char *hasQM(char* str);
 
 mySymbolList *createSymbolNode (char* str, unsigned int dc, int external, int action);
-	mySymbolList *addSymbolNode (mySymbolList* symbolList, char* str, unsigned int dc, int external, int action);
+mySymbolList *addSymbolNode (mySymbolList* symbolList, char* str, unsigned int dc, int external, int action);
