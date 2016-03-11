9815a34f2a7d6e0707ab331ae1ed0b077592019d
Dror Bletter <voodoodror@gmail.com>
2016-03-11 23:33:24 +0200

    Update assembler.c

--- Dror/src/assembler.c
+++ Dror/src/assembler.c
@@ -9,12 +9,14 @@
     const char dotChar = '.';
     const char spaceChar = ' ';
     const char qmChar = '\"';
+    const char newlineChar = '\n';
     const char commaChar = 44;
     int symbolLen = 0;
     int dotLen = 0;
     int symbolCounter = 0;
     int errorFlag = 0;
     int dc=0, ic=0;
+    int count;
     mySymbolList* symbolList;
     myHashTable *hashTable[1000];
     myCommandTable commandTable[16];
@@ -23,14 +25,16 @@
 int main(int argc, char **argv)
 {
     int i;
+
     FILE *fp;
     char file_name[20];
     char *lineToProcess = malloc(sizeof(char) * BUF_SIZE);
+
     init_command_table();
 
     for (i = 1; i < argc; i++)
     {
-        int count = 0;
+
 
         sprintf(file_name, "%s", argv[i]);
         fp = fopen(file_name, "r");
@@ -40,6 +44,8 @@ int main(int argc, char **argv)
             printf("Error: can't find file %s\n", file_name);
             return -1;
         }
+
+        count=0;
 
         while (fgets(lineToProcess, MAX_LINE, fp))
         {
@@ -92,20 +98,20 @@ void first_parsing_line (char *line, int *count) {
 	char *symbolPointer;
 	char *stringFound;
 	char *dotCommand;
-	char tmp[80];
-	char *temp;
-	void *test;
+	char extractResult;
 
 	strip_extra_spaces(line);
 	if (line[0] == ';') {
 		printf("%d: %s (comment line, ignoring)\n",*count,line);
+	} else if (strlen(line) == 0){
+		printf("%d: Empty line found, ignoring\n",*count);
 	} else {
 		if (!isalpha(line[0]) && line[0] != '.') {
-				printf("%d: (must begin with a letter OR a DOT!)\n",*count);
+				printf("%d: (line MUST begin with a letter OR a DOT!)\n",*count);
 		} else {
 			if (hasSymbol(line) != 0) {
 				if (hasSymbol(line+(symbolLen+sizeof(symbolChar))) != 0) {
-					printf("%d: %s (multiple symbols found, ignoring)\n",*count,line);
+					printf("%d: %s (Syntax Error! multiple symbol signs found!)\n",*count,line);
 				} else {
 					if (symbolCounter!=0) {
 						symbolPointer = getSymbol(line,hasSymbol(line));
@@ -113,7 +119,7 @@ void first_parsing_line (char *line, int *count) {
 						for (iter = symbolList; NULL != iter; iter = iter->next) {
 								if(strcmp(iter->Sym,symbolPointer) == 0) {
 										dupSymbol = 1;
-										printf("%d: %s (duplicate symbol found, ignoring... addr? %u action? %i ext? %i)\n",*count,line,iter->addr,iter->action,iter->ext);
+										printf("%d: %s (duplicate symbol found)\n",*count,line,iter->addr,iter->action,iter->ext);
 								}
 						}
 					}
@@ -122,22 +128,13 @@ void first_parsing_line (char *line, int *count) {
 						if (hasDot(line+(symbolLen+sizeof(symbolChar)+sizeof(spaceChar))) != NULL) {
 							dotCommand = getNextString(line+(symbolLen+sizeof(symbolChar)+sizeof(spaceChar)+sizeof(dotChar)));
 							if (strcmp(dotCommand,"string") == 0) {
-								/*line = line+(symbolLen+sizeof(symbolChar)+sizeof(spaceChar)+sizeof(dotChar)+sizeof(dotCommand)+sizeof(spaceChar));
-								extractData(line,dotCommand);
-								while (tmp != NULL) {
-									printf("%s\n",tmp);
-									temp = strtok(NULL,qmChar);
-								}
-								if (line[symbolLen+3+dotLen+1] == '\"' && hasQM(line+(symbolLen+3+dotLen+1)) != NULL) {
-									stringFound = extractString(line+(symbolLen+3+dotLen+1));
-									printf("%d: %s (string found, it is: \"%s\")\n",*count,line,stringFound);
-									} else {
-										printf("%d: %s (string found but does not end properly)\n",*count,line);
-									}*/
-								printf("%d: %s (string found)\n",*count,line);
+								extractResult = extractData(line+(symbolLen+sizeof(symbolChar)+sizeof(spaceChar)+sizeof(dotChar)+strlen(dotCommand)+sizeof(spaceChar)),"string");
+								if (extractResult)
+									printf("%d: %s (string found)\n",*count,line);
 							} else if (strcmp(dotCommand,"data") == 0) {
-								extractData(line,"data");
-								printf("%d: %s (data found)\n",*count,line);
+								extractResult = extractData(line+(symbolLen+sizeof(symbolChar)+sizeof(spaceChar)+sizeof(dotChar)+strlen(dotCommand)+sizeof(spaceChar)),"data");
+								if (extractResult)
+									printf("%d: %s (data found)\n",*count,line);
 							} else {
 								printf("%d: %s (unknown instruction line, ignoring)\n",*count,line);
 							}
@@ -198,17 +195,19 @@ void first_parsing_line (char *line, int *count) {
 
 void strip_extra_spaces(char* str) {
   int i,x;
-  char newlineChar = '\n';
+
   for(i=x=0; str[i]; ++i) {
-	  /*if (isspace(str[i]) && (str[i+1] == 44 || str[i-1] == 44)) {
+	  if (str[i+1] == '\t')
+	  		  str[i+1] = ' ';
+	  if (isspace(str[i]) && !isalpha(str[i-1]) && (str[i-1] != symbolChar) && (!isdigit(str[i+1]) || str[i+1] =='-' || str[i-1] == qmChar)) {
 		  	  i++;
 	  	  }
-	  else*/ if(!isspace(str[i]) || (i>0 && !isspace(str[i-1])))
+	  if(!isspace(str[i]) || (i>0 && !isspace(str[i-1])))
 		  str[x++] = str[i];
-	  else if (str[i+1] == '\t')
-		  str[i+1] = ' ';
 
   }
+  if (isspace(str[x]))
+	  str[x] = '\0';
   if (strchr(str,newlineChar) != NULL) {
 	  str[x-1] = '\0';
   } else {
@@ -241,22 +240,65 @@ char *hasQM(char* str) {
 
 	return tmpStr;
 }
-void extractData(char *str, char *type) {
-	/*char *dataPos;
-	int tmpLen;
-	char *tmp = malloc(sizeof(str));
+int extractData(char *str, char *type) {
 
+	char rwString[BUF_SIZE];
+	char *rwPointer;
+	strcpy(rwString,str);
+	char *token;
+
+	rwPointer = rwString;
+	if (str == NULL) {
+		printf("No data found in string");
+		return 0;
+	}
 	if (strcmp(type,"data") == 0) {
-		dataPos = strchr(str, commaChar);
 
-		while (dataPos != NULL) {
-			dataPos = strchr(str,commaChar);
-			tmpLen = strlen(str)-strlen(dataPos);
-			memcpy(tmp,str,tmpLen+1);
-			tmp[dotLen] = '\0';
+		token = strsep(&rwPointer,",");
+		if (token == NULL) {
+			printf("%d: Error occurred while trying to parse data field.\n1. Please make sure you don\'t have comma at the beginning or end of the string.\n2. Don't put 2 commas side by side.",count);
+			return 0;
 		}
-	} else if (strcmp(type,"string") == 0) {
-	}*/
+		while (token != NULL) {
+				if ((strlen(token)<=1) && (!isdigit(token[0]))) {
+					printf("%d: Syntax Error: Empty or invalid char found.\n",count);
+					return 0;
+				}
+				token = strsep(&rwPointer,",");
+		}
+		strcpy(rwString,str);
+		rwPointer = rwString;
+		token = strsep(&rwPointer,",");
+
+		while (token != NULL) {
+		        printf("TOKEN: %s\n", token);
+		        token = strsep(&rwPointer,",");
+		        dc++;
+		}
+
+		return 1;
+
+		} else if (strcmp(type,"string") == 0) {
+			int qmFound=0;
+			token = str;
+			while (token != NULL) {
+				token = hasQM(token+1);
+				qmFound++;
+			}
+			if (qmFound==2) {
+				str = str+1;
+				str[strlen(str)-sizeof(qmChar)] = '\0';
+				printf("TOKEN: %s\n",str);
+				return 1;
+			} else if (qmFound<=2) {
+				printf("%d: Syntax Error: No quotation marks found for string!\n",count);
+				return 0;
+			} else if (qmFound>2) {
+				printf("%d: Syntax Error: Too much quotation marks found!\n",count);
+				return 0;
+			}
+		}
+	return 0;
 }
 char *getNextString(char* str) {
 	char *dotPos = strchr(str, spaceChar);
@@ -275,7 +317,7 @@ char *getNextString(char* str) {
 	return tmp;
 }
 char *getSymbol(char* str, int pos) {
-	char *myStr;
+	char *myStr = malloc(sizeof(char*));
 	memcpy(myStr,str,pos);
 	myStr[pos] = '\0';
 	return myStr;
